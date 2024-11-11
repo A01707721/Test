53dfc30964cd39d92486696b98086cd8e96dd3d1
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-11-11 07:23:23 -0600

    Update plice.py

--- plice.py
+++ plice.py
@@ -5,3 +5,5 @@ st.title("Police incidents reports from 2018 to 2020")
 
 df=pd.read_csv("Policev1.csv")
 df
+st.markdown("The data shown belongs to incident reports in the city of San Francisco from the year 2018 to 2020")
+
