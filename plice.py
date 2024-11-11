045218a092881dac7986270080803a085e6eb37f
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-11-11 07:33:26 -0600

    Update plice.py

--- plice.py
+++ plice.py
@@ -7,3 +7,15 @@ df=pd.read_csv("Policev1.csv")
 df
 st.markdown("The data shown belongs to incident reports in the city of San Francisco from the year 2018 to 2020")
 
+mapa=pd.DataFrame()
+mapa['Date']=df['Incident Date']
+mapa['Day']=df['Incident Day of Week']
+mapa['Neigborhood']=df['Analysis Neighborhood']
+mapa['Police District']=df['Police District']
+mapa['Incident Category']=df['Incident Category']
+mapa['Incident Subcategory']=df['Incident Subcategory']
+mapa['Resolution']=df['Resolution']
+mapa['lat']=df['Latitude']
+mapa['lon']=df['Longitude']
+mapa=mapa.dropna()
+
